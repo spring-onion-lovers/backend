f1fa670318e47c2b175ddf52ab0dd672dede554d
Nabil Ridhwan <nabridhwan@gmail.com>
2024-06-22 20:32:46 +0800

    feat: add base prisma client usage

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello() {
+  constructor(private prismaService: PrismaService) {}
+
+  async getHello() {
     return {
       message:
-        'Hello World! If you see the env values, this app was set up correctly.',
+        'Hello World! If you see the env values, this app was set up correctly. Each refresh will add a user to the list below. Look at `getUsers`.',
+
       date: new Date().toISOString(),
-      env: JSON.parse(JSON.stringify(process.env || '{}')),
+      envExists: Object.keys(process.env).length > 0,
+      users: await this.getUsers(),
     };
+  }
+
+  async getUsers() {
+    await this.prismaService.user.create({
+      data: {
+        email: `johndoe-${Math.random()}@email.com`,
+        name: 'John Doe',
+      },
+    });
+
+    return this.prismaService.user.findMany();
   }
 }
