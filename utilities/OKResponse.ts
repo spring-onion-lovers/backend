9aa66d8d359026d63d3e14e967bc5351eb89ae87
Nabil Ridhwan <nabridhwan@gmail.com>
2024-06-26 10:37:11 +0800

    feat: add OKResponse helper class for responses

--- utilities/OKResponse.ts
+++ utilities/OKResponse.ts
@@ -2,11 +2,30 @@
  * OKResponse class – a utility class for creating a standard response object
  */
 export default class OKResponse {
+  public message: string = 'OK';
+  public statusCode: number = 200;
   public error: boolean = false;
+  public data: unknown = {};
 
   constructor(
+    data: unknown = {},
+    message: string = 'OK',
+    statusCode: number = 200,
+  ) {
+    this.data = data || {};
+    this.data = data;
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+export class OKResponsePaginated extends OKResponse {
+  constructor(
     public data: unknown = {},
-    public message: string = 'OK',
-    public statusCode: number = 200,
-  ) {}
+    public total_count: number,
+    public page_count: number,
+    public page: number,
+  ) {
+    super(data);
+  }
 }
