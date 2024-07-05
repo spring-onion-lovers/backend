3ab62f9aad7f328241a144da753ae09d00e961c7
Nabil Ridhwan <nabridhwan@gmail.com>
2024-07-05 20:23:46 +0800

    feat: update main.ts to use PORT from env and update env example

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api', { exclude: ['/'] });
   app.useGlobalFilters(new PrismaFilter());
-  await app.listen(3000);
+  await app.listen(process.env.PORT!);
 }
 
 bootstrap();
