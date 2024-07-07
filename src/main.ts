eb8a0743077871b1ea1136ff6a5cf19eddd41e77
Nabil Ridhwan <nabridhwan@gmail.com>
2024-07-08 03:25:10 +0800

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { PrismaFilter } from './prisma/prisma.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api', { exclude: ['/'] });
   app.useGlobalFilters(new PrismaFilter());
   await app.listen(process.env.PORT!);
